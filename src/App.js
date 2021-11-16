1d20fb1056f0564d739ea6e7721634caf8252a0d
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2021-11-17 00:22:15 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
 import React from 'react';
 import "./App.css";
 import Background from "./Background";
- import Example3 from './Example3';
+import Example3 from './Example3';
 
 const App =()=> {
 
         return (
             <div className="app" >
                <Background/>
-				<Example3/>
+	       <Example3/>
             </div>
         );
     }
 
 export default App;
-
-
-
-
-
-
-
-
-// https://www.golangprograms.com/display-json-data-in-reactjs.html
-
-
-           //style={{ 
-                //fonctionne 
-               // backgroundImage:  `url("https://via.placeholder.com/500")` ,
-            //fonctionne pas 
-               //  backgroundImage: `url("http://localhost:3000/background.png")` ,
-      //      backgroundImage:  "url(" + background + ")" ,
-        //    backgroundImage:  `url(${background})` ,
-     //   backgroundImage:  `url("${background}")` ,
-
-           // backgroundRepeat: 'no-repeat',
-          //  width:'500px',
-          //  height:'300px'
-          //   }}
